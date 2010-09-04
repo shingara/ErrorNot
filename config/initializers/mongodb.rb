f05de43dc68006855540a2520ecc31d662113cf7
Cyril Mougel <cyril.mougel@gmail.com>
2010-09-05 00:11:46 +0200

    add option :slave_ok to true on MongoDB connection. Needed by MongoDB >= 1.6.0 and only one server

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -2,5 +2,5 @@ db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
 
 if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
-  MongoMapper.setup(db_config, Rails.env, :logger => Rails.logger)
+  MongoMapper.setup(db_config, Rails.env, :logger => Rails.logger, :slave_ok => true)
 end
