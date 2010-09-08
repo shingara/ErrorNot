65ebac040603d5575456a1362f2aee2a5b3e37c5
Cyril Mougel <cyril.mougel@gmail.com>
2010-09-08 17:34:28 +0200

    start migration to Rails 3

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+gem 'mongoid', '2.0.0.beta.17'
+gem 'bson_ext'
+gem 'haml'
+gem 'devise'
+
+gem 'rspec-rails', ">= 2.0.0.beta.17", :group => [:development, :test]
+
+group :test do
+  gem 'rspec'
+  gem 'randexp'
+  gem 'factory_girl'
+  gem 'factory_girl_rails'
+  gem 'email_spec'
+  gem 'delorean'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Errornot::Application.config.secret_token = 'de4d3b158f6c1d8a712751f52fa27fa444d4358bdda84c12e0e6337d44874148b7bba63d68ed9aac753f8c5de08247336d31fdc66f05e729fbe65cd60dc39b12'
